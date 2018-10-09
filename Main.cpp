9662aa44ff52d2d33872e9445770a45538c18660
hay-dee <null>
2018-10-10 00:21:44 +0100

    Added picture processing implementations without threads

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <assert.h>
 
 #include "Colour.hpp"
 #include "Utils.hpp"
@@ -12,13 +13,10 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-  cout << "------------------------------------------------------" << endl;
-  cout << "TODO: Implement the C++ Picture Processing Interpreter" << endl;
-  cout << "------------------------------------------------------" << endl;
-
   // image files can be passed here. Will need to parse and pre-load them into the
   // PicLibrary class store, using the file's base name as key.
 
+  Utils utils;
   PicLibrary picLib;
 
   // write main IO loop that runs the command-line interpreter interactive shell
@@ -46,27 +44,90 @@ int main(int argc, char ** argv)
 
     } else if (tokens[0] == "load") {
 
+      if (tokens.size() != 3) {
+        cout << "Load usage: load <file_path> <file_name>." << endl;
+        continue;
+      }
+
       picLib.loadpicture(tokens[1], tokens[2]);
 
     } else if (tokens[0] == "unload") {
+
+      if (tokens.size() != 2) {
+        cout << "Unload usage: unload <file_name>." << endl;
+        continue;
+      }
 
       picLib.unloadpicture(tokens[1]);
 
     }  else if (tokens[0] == "save") {
 
+      if (tokens.size() != 3) {
+        cout << "Save usage: save <file_name> <file_path>." << endl;
+        continue;
+      }
+
+      picLib.savepicture(tokens[1], tokens[2]);
+
     }  else if (tokens[0] == "exit") {
+
+      // free stuff here.
+      return 0;
 
     }  else if (tokens[0] == "display") {
 
+      if (tokens.size() != 2) {
+        cout << "Display usage: display <file_name>." << endl;
+        continue;
+      }
+
+      picLib.display(tokens[1]);
+
     }  else if (tokens[0] == "invert") {
+
+      if (tokens.size() != 2) {
+        cout << "Invert usage: invert <file_name>." << endl;
+        continue;
+      }
+
+      picLib.invert(tokens[1]);
 
     }  else if (tokens[0] == "grayscale") {
 
+      if (tokens.size() != 2) {
+        cout << "Grayscale usage: grayscale <file_name>." << endl;
+        continue;
+      }
+
+      picLib.grayscale(tokens[1]);
+
     }  else if (tokens[0] == "rotate") {
+
+      int rotateBy = stoi(tokens[1]);
+      if (tokens.size() != 3 || rotateBy != 90 && rotateBy != 180 && rotateBy != 270) {
+        cout << "Rotate usage: rotate [90|180|270] <file_name>." << endl;
+        continue;
+      }
+
+      picLib.rotate(rotateBy, tokens[2]);
 
     }  else if (tokens[0] == "flip") {
 
+      if (tokens.size() != 3 || tokens[1] != "H" && tokens[1] != "V") {
+        cout << "Flip usage: flip [H|V] <file_name>." << endl;
+        continue;
+      }
+
+      picLib.flipVH(tokens[1][0], tokens[2]);
+
     }  else if (tokens[0] == "blur") {
+
+      if (tokens.size() != 2) {
+        cout << "Blur usage: blur <file_name>." << endl;
+        continue;
+      }
+
+      picLib.blur(tokens[1]);
 
     }  else {
       cout << "Unrecognised command.. Please try again." << endl;
--- PicLibrary.cpp
+++ PicLibrary.cpp
@@ -1,6 +1,8 @@
 #include "PicLibrary.hpp"
 #include "Colour.hpp"
 
+using namespace cv;
+
 void PicLibrary::print_picturestore() {
   for (auto it = picStore.cbegin(); it != picStore.cend(); it++) {
     cout << "Image name: " << it->first << endl;
@@ -29,4 +31,220 @@ void PicLibrary::unloadpicture(string filename) {
   } else {
     cout << "Failed to remove picture: " << filename << endl;
   }
+}
+
+void PicLibrary::savepicture(string filename, string path) {
+  map<string, Picture>::iterator it = picStore.find(filename);
+
+  if (it == picStore.end()) {
+    cout << "Picture: " << filename << " doesn't exist" << endl;
+    return;
+  }
+
+  Utils utils;
+  if (!utils.saveimage(it->second.getimage(), path)) {
+    cout << "Failed to save picture." << endl;
+  }
+}
+
+void PicLibrary::display(string filename) {
+  map<string, Picture>::iterator it = picStore.find(filename);
+
+  if (it == picStore.end()) {
+    cout << "Picture: " << filename << " doesn't exist" << endl;
+    return;
+  }
+
+  Utils utils;
+  utils.displayimage(it->second.getimage());
+}
+
+void PicLibrary::invert(string filename) {
+  map<string, Picture>::iterator it = picStore.find(filename);
+
+  if (it == picStore.end()) {
+    cout << "Picture: " << filename << " doesn't exist" << endl;
+    return;
+  }
+
+  Picture pic = it->second;
+  int width = pic.getwidth();
+  int height = pic.getheight();
+
+  for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+      Colour old_pixel = pic.getpixel(x, y);
+      Colour new_pixel = Colour(255 - old_pixel.getblue(), 255 - old_pixel.getgreen(), 255 - old_pixel.getred());
+      cout << old_pixel.getblue() << " " << new_pixel.getblue() << endl;
+      pic.setpixel(x, y, new_pixel);
+    }
+  }
+
+  cout << "Inverted " << filename << " successfully." << endl;
+}
+
+void PicLibrary::grayscale(string filename) {
+  map<string, Picture>::iterator it = picStore.find(filename);
+
+  if (it == picStore.end()) {
+    cout << "Picture: " << filename << " doesn't exist" << endl;
+    return;
+  }
+
+  Picture pic = it->second;
+  int origWidth = pic.getwidth();
+  int origHeight = pic.getheight();
+
+  for (int x = 0; x < origWidth; x++) {
+    for (int y = 0; y < origHeight; y++) {
+      Colour old_pixel = pic.getpixel(x, y);
+      double avg = (old_pixel.getblue() + old_pixel.getgreen() + old_pixel.getred()) / 3;
+      Colour new_pixel = Colour(avg, avg, avg);
+      pic.setpixel(x, y, new_pixel);
+    }
+  }
+
+  cout << "Grayscaled " << filename << " successfully." << endl;
+}
+
+void PicLibrary::rotate(int angle, string filename) {
+  map<string, Picture>::iterator it = picStore.find(filename);
+
+  if (it == picStore.end()) {
+    cout << "Picture: " << filename << " doesn't exist" << endl;
+    return;
+  }
+
+  Picture pic = it->second;
+  int origWidth = pic.getwidth();
+  int origHeight = pic.getheight();
+  Utils utils;
+
+  switch (angle) {
+    case 90: {
+      Picture new_pic(origHeight, origWidth);
+      for (int x = 0; x < origWidth; x++) {
+        for (int y = 0; y < origHeight; y++) {
+          new_pic.setpixel(origHeight - y - 1, x, pic.getpixel(x, y));
+        }
+      }
+
+      it->second = new_pic;
+      cout << "Rotated " << filename << " by 90deg clockwise successfully." << endl;
+      break;
+    }
+
+    case 180: {
+      Picture new_pic(origWidth, origHeight);
+      for (int x = 0; x < origWidth; x++) {
+        for (int y = 0; y < origHeight; y++) {
+          new_pic.setpixel(origWidth - x - 1, origHeight - y - 1, pic.getpixel(x, y));
+        }
+      }
+
+      it->second = new_pic;
+      cout << "Rotated " << filename << " by 180deg clockwise successfully." << endl;
+      break;
+    }
+
+    case 270: {
+      Picture new_pic(origHeight, origWidth);
+      for (int x = 0; x < origWidth; x++) {
+        for (int y = 0; y < origHeight; y++) {
+          new_pic.setpixel(y, origWidth - x - 1, pic.getpixel(x, y));
+        }
+      }
+
+      it->second = new_pic;
+      cout << "Rotated " << filename << " by 270deg clockwise successfully." << endl;
+      break;
+    }
+  }
+}
+
+void PicLibrary::flipVH(char plane, string filename) {
+
+  map<string, Picture>::iterator it = picStore.find(filename);
+
+  if (it == picStore.end()) {
+    cout << "Picture: " << filename << " doesn't exist" << endl;
+    return;
+  }
+
+  Picture pic = it->second;
+  int origWidth = pic.getwidth();
+  int origHeight = pic.getheight();
+
+  Picture new_pic(origWidth, origHeight);
+
+  switch (plane) {
+    case 'H':
+
+      for (int x = 0; x < origWidth; x++) {
+        for (int y = 0; y < origHeight; y++) {
+          new_pic.setpixel(origWidth - x - 1, y, pic.getpixel(x, y));
+        }
+      }
+
+      it->second = new_pic;
+      cout << "Rotated " << filename << " horizontally successfully." << endl;
+
+      break;
+    case 'V':
+
+      for (int x = 0; x < origWidth; x++) {
+        for (int y = 0; y < origHeight; y++) {
+          new_pic.setpixel(x, origHeight - y - 1, pic.getpixel(x, y));
+        }
+      }
+
+      it->second = new_pic;
+      cout << "Flipped " << filename << " vertically successfully." << endl;
+      break;
+  }
+}
+
+void PicLibrary::blur(string filename) {
+  map<string, Picture>::iterator it = picStore.find(filename);
+
+  if (it == picStore.end()) {
+    cout << "Picture: " << filename << " doesn't exist" << endl;
+    return;
+  }
+
+  Picture pic = it->second;
+  int origWidth = pic.getwidth();
+  int origHeight = pic.getheight();
+
+  Picture new_pic(origWidth, origHeight);
+
+  for (int x = 1; x < origWidth - 1; x++) {
+    for (int y = 1; y < origHeight - 1; y++) {
+      int sumR = 0;
+      int sumG = 0;
+      int sumB = 0;
+      for (int i = -1; i < 2; i++) {
+        for (int j = -1; j < 2; j++) {
+          Colour pixel = pic.getpixel(x + i, y + j);
+          sumR += pixel.getred();
+          sumG += pixel.getgreen();
+          sumB += pixel.getblue();
+        }
+      }
+      new_pic.setpixel(x, y, Colour(sumR / 9, sumG / 9, sumB / 9));
+    }
+
+    for (int x = 0; x < origWidth; x++) {
+      new_pic.setpixel(x, 0, pic.getpixel(x, 0));
+      new_pic.setpixel(x, origHeight - 1, pic.getpixel(x, origHeight - 1));
+    }
+
+    for (int y = 1; y < origHeight - 1; y++) {
+      new_pic.setpixel(0, y, pic.getpixel(0, y));
+      new_pic.setpixel(origWidth - 1, y, pic.getpixel(origWidth - 1, y));
+    }
+  }
+  
+  it->second = new_pic;
+  cout << "Blured " << filename << " successfully." << endl;
 }
